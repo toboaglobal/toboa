58914d7502fe225a7de467a661ae8536b3cf90c4
toboaglobal <87712253+toboaglobal@users.noreply.github.com>
2021-08-01 23:54:06 +0530

    Add files via upload

---
+++ toboaPage/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url
+from toboaPage import views
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+]
